66d248d757f3e1f0864180919831c9c4fac2e117
Olukeye <seunolukeye2000@gmail.com>
2021-07-28 12:54:44 +0100

    included a login api

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const User = require('../models/User');
+const CryptoJS = require("crypto-js");
+const jwt = require('jsonwebtoken');
 
-
-// 
+// Register user
 router.post('/register', async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password
-    })
-
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY ).toString()
+    });
    try {
 
     const user = await newUser.save();
@@ -19,6 +19,32 @@ router.post('/register', async (req, res) => {
        res.status(500).json(err);
    }
 });
+
+// Login user
+router.post('/login', async (req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+
+        // if is not a user 
+        !user && res.status(400).json("Wrong email or password!");
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        // if login password doesn't match the original password .....
+        originalPassword !== req.body.password && res.status(400).json('Wrong email or password!')
+
+        // accessToken
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin }, process.env.SECRET_KEY, {expiresIn: "2"} );
+        const {password, ...info } = user._doc;  //Hide user password 
+
+        res.status(200).json({...info, token})
+
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
 
 
 module.exports = router
--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const db      = require('./db/mongoose')
 const authRoute      = require('./routes/auth')
-
-
-
+const dotenv = require('dotenv');
+const cors = require('cors');
 
 const app = express();
 
+
+// Load env vars
+dotenv.config();
+
+
+// Body parser
 app.use(express.json());
 
-
+// Enable cors
+app.use(cors());
 
 
 app.use("/api/auth", authRoute)
+/** Mount Routers */
+app.use("/api/auth", authRoute)
+
 
 
 const port = process.env.PORT || 2021
